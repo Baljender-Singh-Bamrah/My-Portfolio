bb1ba1cba63dd7281f3f78e3f4328c1060e3cfe8
Baljender Singh Bamrah <156078007+Baljender-Singh-Bamrah@users.noreply.github.com>
2025-05-06 00:26:24 +0530

    first commit

--- app/page.js
+++ app/page.js
@@ -1,10 +1,16 @@
 import { personalData } from "@/utils/data/personal-data";
+import dynamic from 'next/dynamic';
 import AboutSection from "./components/homepage/about";
-import ContactSection from "./components/homepage/contact";
 import Education from "./components/homepage/education";
 import HeroSection from "./components/homepage/hero-section";
 import Projects from "./components/homepage/projects";
 import Skills from "./components/homepage/skills";
+
+// Dynamically import the contact section with no SSR
+const ContactSection = dynamic(
+  () => import('./components/homepage/contact'),
+  { ssr: false }
+);
 
 async function getData() {
   const res = await fetch(`https://dev.to/api/articles?username=${personalData.devUsername}`)
@@ -24,7 +30,7 @@ export default async function Home() {
   const blogs = await getData();
 
   return (
-    <div suppressHydrationWarning >
+    <div suppressHydrationWarning>
       <HeroSection />
       <AboutSection />
       <Skills />
